89018739a799cef1c522757809d2ff763d4541c2
Kasper Holbek Jensen <gh@kasper.codes>
2024-11-07 06:50:42 +0100

    Remove old sitemap

--- edit.js
+++ edit.js
@@ -48,48 +48,7 @@ document.getElementById('preview').addEventListener('click', function() {
   });
 });
 
-// Traverse links, fetch resources, traverse those links, until sitemap is filled in.
-// This doesn't need to be this complex at all once we use a bounded file system.
-async function generateSiteMap(doc) {
-  siteMap = new Map();
-
-  links = getLinks(doc);
-  for (const link of links) {
-    try {
-      const response = await fetch(link);
-      if (response.headers.get('Content-Type').includes('text/html')) {
-        const resourceHtml = await response.text();
-        const resourceDoc = new DOMParser().parseFromString(resourceHtml, 'text/html');
-        replaceStaticLinks(resourceDoc, siteMap);
-        siteMap.set(link, URL.createObjectURL(new Blob([resourceDoc.documentElement.outerHTML], { type: "text/html" })));
-      } else if (link.endsWith('.md')){
-        // Just get static links from markdown (images, etc)
-        const resourceText = await response.text();
-        const links = resourceText.match(/!\[.*?\]\((.*?)\)/g)?.map(match => match.match(/\((.*?)\)/)[1]);
-        if (links) {
-          relevantLinks = links.filter(link => !link.startsWith('http')).filter(link => !link.endsWith('.md') || link.endsWith('.html'))
-          for (const link of relevantLinks) {
-            const response = await fetch(link)
-            blob = await response.blob()
-            siteMap.set(link, URL.createObjectURL(blob));
-          }
-        }
-        const resourceBlob = new Blob([resourceText], { type: "text/markdown" });
-        const resourceUrl = URL.createObjectURL(resourceBlob);
-        siteMap.set(link, resourceUrl);
-      } else {
-        const resourceBlob = await response.blob();
-        const resourceUrl = URL.createObjectURL(resourceBlob);
-        siteMap.set(link, resourceUrl);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  }
-  replaceStaticLinks(doc, siteMap);
-  siteMap.set('index.html', URL.createObjectURL(new Blob([doc.documentElement.outerHTML], { type: "text/html" })));
-}
-
+// This generates blobs for all files, then stores them in a map
 async function generateSiteMapFromList(files) {
   siteMap = new Map();
 

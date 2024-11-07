afcaf06f33bbd54d51fafe7645a0f314f16b3987
Kasper Holbek Jensen <gh@kasper.codes>
2024-11-07 11:02:13 +0100

    Nice editor

--- edit.js
+++ edit.js
@@ -1,8 +1,12 @@
+// Editing should be done in dark mode
+document.querySelector('html').setAttribute('data-theme', 'dark');
+// Beginning of Editor
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/dracula");
 
 loadFiles();
 
+// Load the zip file, open index.html in the editor
 async function loadFiles() {
   const response = await fetch("/rawtastic-main.zip");
   const blob = await response.blob();
@@ -10,27 +14,50 @@ async function loadFiles() {
   await zip.loadAsync(blob);
   const files = zip.files;
   const fileNames = Object.keys(files);
-  window.files = files;
+  filemap = new Map();
+  for (const fileName of fileNames) {
+    if (fileName.endsWith('.jpg') || fileName.endsWith('.jpeg') || fileName.endsWith('.png') || fileName.endsWith('.gif')) {
+      filemap.set(fileName, await files[fileName].async('blob'));
+    } else {
+      filemap.set(fileName, await files[fileName].async('text'));
+    }
+  };
+
+  window.files = filemap;
+
 
   // Load index.html
-  const indexHtml = await files['index.html'].async('text');
+  document.getElementById('editor').dataset.currentFile = 'index.html';
+  const indexHtml = filemap.get('index.html');
   editor.setValue(indexHtml);
   editor.session.setMode("ace/mode/html");
 
-  const fileLinks = document.createElement('div');
+  const fileLinks = document.createElement('ul');
+  const br = document.createElement('br');
   fileLinks.id = 'fileLinks';
-  document.getElementById('editor').after(fileLinks);
+  document.getElementById('controls').after(br);
+  br.after(fileLinks);
   for (const fileName of fileNames.filter(fileName => fileName.endsWith('.html') || fileName.endsWith('.css') || fileName.endsWith('.js') || fileName.endsWith('.md'))) {
     const link = document.createElement('a');
     link.href = '#';
     link.innerText = fileName;
+    const languageModes = {
+      'html': 'html',
+      'css': 'css',
+      'js': 'javascript',
+      'md': 'markdown'
+    };
+    const languageMode = languageModes[fileName.split('.').pop()];
+
     link.onclick = async () => {
-      const file = files[fileName];
-      const text = await file.async('text');
+      const text = window.files.get(fileName);
+      console.log(text);
       editor.setValue(text);
-      editor.session.setMode("ace/mode/" + fileName.split('.').pop());
+      console.log(`Setting mode to ace/mode/${languageMode}`);
+      editor.session.setMode("ace/mode/" + languageMode);
+      document.getElementById('editor').dataset.currentFile = fileName;
     }
-    var p = document.createElement('p');
+    var p = document.createElement('li');
     p.appendChild(link);
     fileLinks.appendChild(p);
   }
@@ -38,21 +65,24 @@ async function loadFiles() {
   return files;
 }
 
-
-
-document.getElementById('preview').addEventListener('click', function() {
-  generateSiteMapFromList(window.files).then(() => {
-    localStorage.setItem('siteMap', JSON.stringify(Array.from(siteMap.entries())));
-    var url = siteMap.get('/index.html');
-    window.open(url, '_blank').focus();
-  });
+// Save file on press save
+document.getElementById('save').addEventListener('click', function() {
+  const fileName = document.getElementById('editor').dataset.currentFile;
+  console.log(fileName);
+  content = editor.getValue();
+  window.files.set(fileName, content);
 });
+
+
+// End of Editor
+
+// Begining of Preview Generation
 
 // This generates blobs for all files, then stores them in a map
 async function generateSiteMapFromList(files) {
   siteMap = new Map();
 
-  fileNames = Object.keys(files);
+  fileNames = files.keys().toArray();;
   nonDirs = fileNames.filter(fileName => !fileName.endsWith('/'));
 
   // Ignore html for now
@@ -61,11 +91,11 @@ async function generateSiteMapFromList(files) {
     address = `/${fileName}`;
 
     if (type === 'css' || type === 'js') {
-      siteMap.set(address, URL.createObjectURL(new Blob([await files[fileName].async('text')], { type: "text/" + type })));
+      siteMap.set(address, URL.createObjectURL(new Blob([files.get(fileName)], { type: "text/" + type })));
     } else if (type === 'md') {
-      siteMap.set(address, URL.createObjectURL(new Blob([await files[fileName].async('text')], { type: "text/markdown" })));
+      siteMap.set(address, URL.createObjectURL(new Blob([files.get(fileName)], { type: "text/markdown" })));
     } else if (type === 'png' || type === 'jpg' || type === 'jpeg' || type === 'gif') {
-      siteMap.set(address, URL.createObjectURL(new Blob([await files[fileName].async('blob')], { type: "image/" + type })));
+      siteMap.set(address, URL.createObjectURL(new Blob([files.get(fileName)], { type: "image/" + type })));
     } else {
       console.error('Unknown file type: ' + type);
     }
@@ -73,27 +103,13 @@ async function generateSiteMapFromList(files) {
 
   // Then do html, but replacing static links
   for (var fileName of nonDirs.filter(name => name.endsWith('.html'))) {
-    const html = await files[fileName].async('text');
+    const html = files.get(fileName);
     doc = new DOMParser().parseFromString(html, 'text/html');
     replaceStaticLinks(doc, siteMap);
     siteMap.set(`/${fileName}`, URL.createObjectURL(new Blob([doc.documentElement.outerHTML], { type: "text/html" })));
   }
 
   return siteMap;
-}
-
-// Only get relative links
-function getLinks(doc) {
-  const elements = doc.querySelectorAll('img[src], script[src], link[href], a[href]');
-  const links = [];
-  for (const element of elements) {
-    const attr = element.src ? 'src' : 'href';
-    const url = element.getAttribute(attr);
-    if (url && !url.startsWith('http')) {
-      links.push(url);
-    }
-  }
-  return links;
 }
 
 function replaceStaticLinks(doc, siteMap) {
@@ -110,3 +126,13 @@ function replaceStaticLinks(doc, siteMap) {
     }
   }
 }
+
+document.getElementById('preview').addEventListener('click', function() {
+  generateSiteMapFromList(window.files).then(() => {
+    localStorage.setItem('siteMap', JSON.stringify(Array.from(siteMap.entries())));
+    var url = siteMap.get('/index.html');
+    window.open(url, '_blank').focus();
+  });
+});
+
+// End of Preview Generation
